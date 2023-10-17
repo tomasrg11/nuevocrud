5fcb846971f5706d91822d2a4655d94bda8f2feb
tomasrg11 <tomramirezg11@gmail.com>
2023-10-17 18:52:07 -0500

    Se agrego las imagenes

--- js/app.js
+++ js/app.js
@@ -42,6 +42,7 @@ const cargarTabla = () => {
         <td>${item.tipo}</td>
         <td>${item.tiempo}</td>
         <td>$${item.precio}</td>
+        <td><img src="${item.imagen}" alt="${item.gift}" width="40" height="55"></td>
         <td>
         <div class="d-flex gap-2">
         <button class="btn btn-outline-warning" onclick="mostrarModal(${item.id})"><i class="fa fa-pencil" aria-hidden="true"></i></button>
